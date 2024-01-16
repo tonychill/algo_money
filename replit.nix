41a495551c7f028cc0f9d7b461319c0a7c31ceb2
quedogf94 <quedogf94@yahoo.com>
2024-01-16 05:40:41 +0000

    Change to replit

--- replit.nix
+++ replit.nix
@@ -1,5 +1,16 @@
-{ pkgs }: {
-    deps = [
-      pkgs.cowsay
-    ];
+{pkgs}: {
+  deps = [
+    pkgs.glibcLocales
+    pkgs.glibc
+    pkgs.tk
+    pkgs.tcl
+    pkgs.qhull
+    pkgs.pkg-config
+    pkgs.gtk3
+    pkgs.gobject-introspection
+    pkgs.ghostscript
+    pkgs.freetype
+    pkgs.ffmpeg-full
+    pkgs.cairo
+  ];
 }
